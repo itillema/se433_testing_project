ef177ce8beb9a9168b9f06b25cb026838b7a6327
Isaac Tillema <tillema97@gmail.com>
2026-05-07 09:56:36 -0500

    exam work

--- app/src/main/java/se433_testing_project/ShippingOption.java
+++ app/src/main/java/se433_testing_project/ShippingOption.java
@@ -6,5 +6,15 @@ public enum ShippingOption
     NEXT_DAY
 }
 
+public class Shipping(String option) 
+{
+    if (option.equalsIgnoreCase("standard")) {
+        return ShippingOption.STANDARD;
+    } else if (option.equalsIgnoreCase("next_day")) {
+        return ShippingOption.NEXT_DAY;
+    } else {
+        throw new IllegalArgumentException("Invalid shipping option: " + option);
+    }
+}
 
 
